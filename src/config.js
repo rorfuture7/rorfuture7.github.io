634c616e36853e26130b1df4ef9100f68285a83a
Arvind Kushwah <arvindkushwah9@gmail.com>
2024-10-24 11:10:11 +0530

    Update ngrok url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001/"
+export const BASE_URL = "https://7d81-103-219-229-188.ngrok-free.app/"
 export const NEWS_API_KEY = "d3f20145ae554a0aa1fe4d61c3ba0545"
 // const user_data = localStorage.getItem('login_user');
 // const current_user = JSON.parse(user_data);
